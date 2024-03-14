5a47c7d9b044e458adb6170cb9b2231ee45fb0fd
Tr4c3 <116485904+keerthiprabup@users.noreply.github.com>
2024-03-14 16:16:43 +0530

    Update start.sh

--- BitVault/start.sh
+++ BitVault/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t bitvault .
 # docker run  -d -p80:80 -p8080:8080 -p22:22 --name bit_container bitvault
-docker run -d --name bit_container --network=host -p2222:22 bitvault 
+docker run -d --name bit_container --network=host bitvault 
