e62557cb2c80ed3270798ade79906868c0aad730
Tr4c3 <116485904+keerthiprabup@users.noreply.github.com>
2024-03-31 14:05:40 +0530

    Update check.sh: updated process kill logic for tomcat server

--- Finale/BitVault/Deployment/check.sh
+++ Finale/BitVault/Deployment/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -f http://localhost:8080 || (pkill -fu root "sudo -u Hax_BitVault env JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre /usr/local/tomcat/bin/catalina.sh run" && sudo -u Hax_BitVault env JAVA_HOME='/usr/lib/jvm/java-8-openjdk-amd64/jre' /usr/local/tomcat/bin/catalina.sh run) 
+curl -f http://localhost:8080 || ((pkill -fu root "sudo -u Hax_BitVault env JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre /usr/local/tomcat/bin/catalina.sh run"||true) && sudo -u Hax_BitVault env JAVA_HOME='/usr/lib/jvm/java-8-openjdk-amd64/jre' /usr/local/tomcat/bin/catalina.sh run) 
 curl -f http://localhost:80 || nginx -g "daemon off;"
 curl -f http://localhost:5000 || python3 /root/app/app.py
 service ssh status || service ssh start 
